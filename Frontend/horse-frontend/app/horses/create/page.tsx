9f5e45f7ef5f7b42055084f922241133e2be96a2
Simon Konieczny <konieczny.szymon@icloud.com>
2026-03-30 20:26:18 +0200

    remove the old '/create' route

--- Frontend/horse-frontend/app/horses/create/page.tsx
+++
@@ -1,7 +0,0 @@
-import CreateHorseForm from "@/components/CreateHorseForm/CreateHorseForm";
-import { getAllHorses } from "@/lib/horses";
-
-export default async function CreateHorsePage(){
-    const horses = await getAllHorses();
-    return <CreateHorseForm horses={horses}/>
-} 
